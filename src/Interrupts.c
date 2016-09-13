99e3c5fd4c07f4d55bd7d640f18ba660ce2c43f7
claudioasfilho <claudioasfilho@gmail.com>
2016-09-13 15:45:42 -0700

    Attempt to add Timer2 but it doesn't work as expected

--- src/Interrupts.c
+++ src/Interrupts.c
@@ -12,7 +12,6 @@
 SI_SBIT (TEST1,SFR_P3, 3);			   //Test LED
 #define ToogleTest1() TEST1^=1;
 
-
 //-----------------------------------------------------------------------------
 // TIMER0_ISR
 //-----------------------------------------------------------------------------
@@ -27,14 +26,13 @@ SI_INTERRUPT (TIMER0_ISR, TIMER0_IRQn)
 
 	TCON &= 0xdf; //Clear the interrupt Flag
 
-	TCON |= 0x40; //Timer 1 Run
-
+	TCON |= 0x40;//Timer 1 Run
 
 //	ToogleTest1();
 
 	if (TL1>10)
 	{
-		ToogleTest1();
+		//	ToogleTest1();
 		TL1=0;
 		TH1=0;
 	}
@@ -42,5 +40,38 @@ SI_INTERRUPT (TIMER0_ISR, TIMER0_IRQn)
 //	TL1=0;
 //	TH1=0;
 
+}
+
+//-----------------------------------------------------------------------------
+// ADC0EOC_ISR
+//-----------------------------------------------------------------------------
+//
+// ADC0EOC ISR Content goes here. Remember to clear flag bits:
+
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (ADC0EOC_ISR, ADC0EOC_IRQn)
+{
+
+
+}
+
+//-----------------------------------------------------------------------------
+// TIMER2_ISR
+//-----------------------------------------------------------------------------
+//
+// TIMER2 ISR Content goes here. Remember to clear flag bits:
+// TMR2CN0::TF2H (Timer # High Byte Overflow Flag)
+// TMR2CN0::TF2L (Timer # Low Byte Overflow Flag)
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (TIMER2_ISR, TIMER2_IRQn)
+{
+
+/*	SFRPAGE = 0x00;
+	TMR2L = 0x0;
+	TMR2H=0x00;*/
+
+	ToogleTest1();
 }
 
--- src/myProject_main.c
+++ src/myProject_main.c
@@ -13,23 +13,6 @@ uint8_t delay;
 int main(void) {
 	// Call hardware initialization routine
 
-#if 0
-	//All Pins are digital except P02
-	P0MDIN = 0xfb;
-	//P00 latched to 1
-	P0 = 0x01;
-	//P00 is a Push Pull
-	P0MDOUT = 0x01;
-
-	//PBCFG_0_enter_DefaultMode_from_RESET();
-
-	//Comparator 0 Setup
-	CMP0CN0 = 0xc0;
-	CMP0MD = 0x0c;
-	CMP0MX = 0xf2;
-	CMP0CN1 = 0x1f;//Half Scale on the DAC
-#endif
-
 	enter_DefaultMode_from_RESET();
 
 	SetDACOutput(0x7ff);
